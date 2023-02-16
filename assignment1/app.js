4d357fdd9e79425db7529e5836d771885da00991
Gireesha <gireesha_holla@apple.com>
2023-02-16 13:26:44 -0800

    cosmetic changes

--- assignment1/app.js
+++ assignment1/app.js
@@ -4,25 +4,30 @@
   angular.module('LunchCheck',[])
   .controller('LunchCheckController',LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope','$filter'];
-  function LunchCheckController($scope, $filter){
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
     $scope.foodStr="";
     $scope.message= "";
     $scope.footNote ="";
+    $scope.foodCount= -1;
     $scope.checkFoodItems = function () {
       $scope.footNote ="";
-      var foodCount = $scope.foodStr.split(',').filter(word => word.trim().length > 0).length;
+      $scope.foodCount = $scope.foodStr.split(',').filter(word => word.trim().length > 0).length;
       //$scope.message = "Your are eating " + foodCount + " food items. ";
-      if (foodCount <=3 ) {
-        if(foodCount==0)
-          $scope.message = "Please enter data first.";
-          else
+      $scope.status="success";
+
+      if ($scope.foodCount <=3 ) {
+        if($scope.foodCount==0) {
+          $scope.message = "Please enter data first";
+          $scope.status="danger";
+        }
+        else
           $scope.message = "Enjoy!";
         } else {
           $scope.message = "Too much!";
         };
 
-        if(foodCount < $scope.foodStr.split(',').length){
+        if($scope.foodCount < $scope.foodStr.split(',').length){
           $scope.footNote = " Note: empty items like ,, or , , are not counted.";
         }
     };
